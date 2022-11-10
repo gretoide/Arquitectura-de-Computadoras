b093d93552d16d70738e22f2c64ea20d5d24065b
gretoide <gretaxpiacentini@gmail.com>
2022-11-10 12:17:06 -0300

    ✨ funcional

--- ejercicios-asm/practica4-procesadorRisc/p4_6.s
+++ ejercicios-asm/practica4-procesadorRisc/p4_6.s
@@ -4,21 +4,22 @@
 
 .data
 a: .word 2 ; r1
-b: .word 1 ; r2
+b: .word 2 ; r2
 c: .word 2 ; r3
-d: .word 0 ; r4
+d: .word 2 ; r4 inicializa en 2
 .code
 ;cargamos variables
-ld r1, a(r0)
-ld r2, b(r0)
-ld r3, c(r0)
-ld r4, d(r0)
+ld r1, a(r0) ;2
+ld r2, b(r0) ;2
+ld r3, c(r0) ;2
+ld r4, d(r0) ;2
 ;comparamos
-bne r1,r2, dos
-daddi r4,r4,1
-dos: bne r2,r3, tres
-daddi r4,r4,1
-tres: bne r1,r3,fin
-daddi r4,r4,1
+beq r1, r2, sumar1 ; si son iguales salta a sumar1, compara el último y termina de sumar
+beq r2, r3, fin    ; si son iguales salta a fin, porque solo hay dos iguales
+beq r1, r3, fin    ; si son iguales salta a fin
+daddi r4, r4, -2   ;si llego hasta acá es porque ninguno es igual
+j fin
+sumar1: beq r1, r3, total ;hace la última comparación
+j fin
+total: daddi r4, r4, 1  ;suma 1, quedan 3 iguales
 fin: halt
- ;ANDA COMO QUIERE, NO SE GUIEN (?)
