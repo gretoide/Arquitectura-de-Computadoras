9a431dc30de6586a2c92a59a7cd881018961a814
gretoide <gretaxpiacentini@gmail.com>
2022-11-12 18:54:06 -0300

    ✨

--- ejercicios-asm/practica4-procesadorRisc/p4_10.s
+++ ejercicios-asm/practica4-procesadorRisc/p4_10.s
@@ -15,17 +15,17 @@ cant: .word 0 ; cantidad de veces que se repite el caracter car en cadena. (8)
 
 .code
 ;cargo variables
-daddi $t4, $zero, $zero  ;inicializo en 0 el puntero de la cadena
+daddi $t4, $zero, 0  ;1)inicializo en 0 el puntero de la cadena
 daddi $t5, $t5, 16 ;cantidad de caracteres de la cadena (para decrementar)
-daddi $a0, $zero, cadena ;1)copio la direccion de cadena en $a0
+daddi $a0, $zero, cadena ;2)copio la direccion de cadena en $a0
 ld $t2, car($zero) ;caracter a  buscar
 ld $t1, cant($zero) ;contador de repeticiones
-dadd $t4, $a0, $zero ;2)copio la direccion de inicio de la cadena (para ir sacando caracteres)
+dadd $t4, $a0, $zero ;3)copio la direccion de inicio de la cadena (para ir sacando caracteres)
 
 ;loop que recorre la cadena
-loop: lbu $t6, 0($t4) ;saco de la cadena el primer caracter (guarda en $t6, desplazamientos 0, el valor de la direccion de $t4)
-daddi $t4, $t4, 1  ;incremento para obtener el siguiente elemento 
-daddi $t6, $t6, -1 ;decremento la longitud del string recorrida
+loop: lbu $t6, 0($t4) ;4) saco de la cadena el primer caracter (guarda en $t6, desplazamientos 0, el valor de la direccion de $t4)
+daddi $t4, $t4, 1  ;5) incremento para obtener el siguiente elemento 
+daddi $t5, $t5, -1 ;decremento la longitud del string recorrida
 beq $t6, $t2, suma  ;si son iguales
 beqz $t5, fin  ;si llegamos a cero salta a fin
 j loop
