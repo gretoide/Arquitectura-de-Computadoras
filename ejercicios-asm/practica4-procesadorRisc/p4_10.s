8f9e22038c9f907f3be3a1826801756dbe4d43d7
gretoide <gretaxpiacentini@gmail.com>
2022-11-12 18:38:39 -0300

    ✅

--- ejercicios-asm/practica4-procesadorRisc/p4_10.s
+++ ejercicios-asm/practica4-procesadorRisc/p4_10.s
@@ -1,33 +1,40 @@
 ; 10) Escribir un programa que cuente la cantidad de veces que un 
-; determinado caracter aparece en una cadena de texto. Observar
-; cómo se almacenan en memoria los códigos ASCII de los caracteres 
-; (código de la letra “a” es 61H). Utilizar la instrucción lbu
-; (load byte unsigned) para cargar códigos en registros. 
-; La inicialización de los datos es la siguiente: .data
-; cadena: .asciiz "adbdcdedfdgdhdid" ; cadena a analizar
-; car: .asciiz "d" ; caracter buscado
-; cant: .word 0 ; cantidad de veces que se repite el caracter car en cadena.
+;   determinado caracter aparece en una cadena de texto. Observar
+;   cómo se almacenan en memoria los códigos ASCII de los caracteres 
+;   (código de la letra “a” es 61H). Utilizar la instrucción lbu
+;   (load byte unsigned) para cargar códigos en registros. 
+;    La inicialización de los datos es la siguiente: .data
+;   cadena: .asciiz "adbdcdedfdgdhdid" ; cadena a analizar
+;   car: .asciiz "d" ; caracter buscado
+;   cant: .word 0 ; cantidad de veces que se repite el caracter car en cadena.
 
 .data
-cadena: .asciiz "adbdcdedfdgdhdid" ;cadena a analizar
+cadena: .asciiz "adbdcdedfdgdhdid" ; cadena a analizar 
 car: .asciiz "d" ; caracter buscado
-cant: .word 0 ; cantidad de veces que se repite el caracter car en cadena.
+cant: .word 0 ; cantidad de veces que se repite el caracter car en cadena. (8)
 
 .code
-daddi r4, r4, 0  ; inicializo en cero
-daddi r5, r5, 16  ; inicializo en 16
-ld r1, cadena(r0) 
-ld r2, car(r0) ; caracter a  buscar
-ld r3, cant(r0); contador de repeticiones
-loop: lbu r6, r1(r4) ; saco de la cadena el primer caracter
-daddi r4, r4, 8  ; incremento para obtener el siguiente elemneto 
-daddi r5, r5, -1 ; decremento la longitud del string
-beq r6, r2, suma  ; si son iguales
-beqz r5, fin  ; si es cero salta a fin
+;cargo variables
+daddi $t4, $zero, $zero  ;inicializo en 0 el puntero de la cadena
+daddi $t5, $t5, 16 ;cantidad de caracteres de la cadena (para decrementar)
+daddi $a0, $zero, cadena ;1)copio la direccion de cadena en $a0
+ld $t2, car($zero) ;caracter a  buscar
+ld $t1, cant($zero) ;contador de repeticiones
+dadd $t4, $a0, $zero ;2)copio la direccion de inicio de la cadena (para ir sacando caracteres)
+
+;loop que recorre la cadena
+loop: lbu $t6, 0($t4) ;saco de la cadena el primer caracter (guarda en $t6, desplazamientos 0, el valor de la direccion de $t4)
+daddi $t4, $t4, 1  ;incremento para obtener el siguiente elemento 
+daddi $t6, $t6, -1 ;decremento la longitud del string recorrida
+beq $t6, $t2, suma  ;si son iguales
+beqz $t5, fin  ;si llegamos a cero salta a fin
 j loop
-suma: daddi r3, r3, 1
-beqz r5, fin  ; si es cero salta a fin
-j loop
+
+;suma de elementos iguales
+suma: daddi $t3, $t3, 1
+beqz $t5, fin  ; si es cero salta a fin; tambien se podria preguntar
+j loop        ; si el ultimo caracter sacado es cero
+nop
 fin: halt
 ; bnez ; si no es cero "0"
-; beqz ; si es cero "0"
+; bnqz ; si es cero "0"
