70c6a91a1319b5b3f093ae845a5d25ae4bbe35cf
gretoide <gretaxpiacentini@gmail.com>
2022-11-20 15:35:52 -0300

    ✅

--- ejercicios-asm/practica6-E/p4_1.s
+++ ejercicios-asm/practica6-E/p4_1.s
@@ -1,6 +1,7 @@
 ;1) El siguiente programa produce la salida de un mensaje predefinido en la ventana Terminal del simulador 
 ;WinMIPS64. Teniendo en cuenta las condiciones de control del puerto de E/S (en el resumen anterior), modifique el 
 ;programa de modo que el mensaje a mostrar sea ingresado por teclado en lugar de ser un mensaje fijo. 
+
 .data
 control: .word 0x10000
 data: .word 0x10008
@@ -12,11 +13,11 @@ ld $s0, control($zero) ;
 ld $s1, data($zero)
 
 ;inicializamos s4 = enter para cortar con eso 
-daddi $s4,$zero,13 
+daddi $s4,$zero,13 ;13 = ascii del enter
 
       ;le mandamos un 9 a control, para que ingrese un caracter
 loop: daddi $t1,$zero,9 
-    sd $t1,0($s0) 
+    sd $t1,0($s0) ;le mandamos el 9 a control
     
     ;nos guardamos el caracter en data
     ld $t1,0($s1)
